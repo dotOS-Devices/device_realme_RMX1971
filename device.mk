92de2164c034af6f703bb0f95d8c1075fb66f99c
ZeeLog <Danjel.Zammit@gmail.com>
2020-02-28 09:58:29 +0100

    RMX1971: Build display config (1-9) extra vendor libs
    Change-Id: Id3092139212732c572670460bb8d5e5461163533

--- device.mk
+++ device.mk
@@ -128,7 +128,16 @@ PRODUCT_PACKAGES += \
     memtrack.sdm710 \
     libtinyxml \
     libvulkan \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.2 \
+    vendor.display.config@1.3 \
+    vendor.display.config@1.4 \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.6 \
     vendor.display.config@1.7 \
+    vendor.display.config@1.8 \
+    vendor.display.config@1.9 \
     vendor.qti.hardware.display.allocator@1.0-service
 
 
