8a259a9e2ddfff1421c0b2e83996dbf74d062d4e
Nicholas Ambur <nambur@google.com>
2020-03-02 15:05:49 -0800

    RMX1971: Add SoundTrigger HAL v2.3 support
    Bug: 139071862
    Test: gts-tradefed run gts-dev -m GtsAssistIntentTestCases
    Change-Id: I2d17aade762ceb7800d5305119c37ec0f5991f35
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -35,7 +35,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
