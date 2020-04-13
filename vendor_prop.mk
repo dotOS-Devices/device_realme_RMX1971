8d3556b0f5e25984db1e565887bb0d5f15b1d50b
Pranav Vashi <neobuddy89@gmail.com>
2020-04-13 13:13:50 +0530

    RMX1971: Add props for graphics
    * Kanged few from Pixel 4 and RazerPhone
    * Some of these are already enabled in vendor partition.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: I9b09e91f840897e4d95b5dc76527d42642481b6f

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,7 +132,22 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.sf.hw=1 \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
