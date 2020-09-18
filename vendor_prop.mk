b7f10e8e7c0f4a5cca02124aebc9d986bc7ef961
Danny Lin <danny@kdrag0n.dev>
2020-09-18 11:58:27 -0700

    RMX1971: Use EXPENSIVE_RENDERING power hint for blurs
    We need to boost the GPU for smooth high-radius blurs as blurring is an
    expensive rendering job.
    
    Change-Id: Ia3968313601d2b04ce50c01ee61dc9944ee26187
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,7 +126,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
