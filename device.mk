d097510034949c1dbfa7db26ca75dde5915a4851
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:04:47 -0800

    RMX1971: Build QuickAccessWallet plugin for SystemUI
    This plugin that provides the wallet (cards and passes) integration in
    the power menu is now open-source, so let's build it.
    
    Change-Id: I5df8a09c8f91805df538bc7ddad6d326f5d9b13e
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -425,6 +425,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# Wallet
+PRODUCT_PACKAGES += \
+    QuickAccessWallet
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
