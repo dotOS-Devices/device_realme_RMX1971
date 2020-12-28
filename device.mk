55383ade7dfb2ec7453190d79c874a85a32d568b
Ethan Chen <intervigil@gmail.com>
2020-12-28 08:23:16 +0100

    RMX1971: Remove unnecessary netutils packages
    * These are already packaged by AOSP.

--- device.mk
+++ device.mk
@@ -205,10 +205,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-# Net
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage \
