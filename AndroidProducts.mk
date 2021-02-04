e6522535e341759b3f2070b93d7821580b64ec74
lucky <ah440099@gmail.com>
2021-02-04 21:57:53 +0200

    RMX1971: BringUp Dotos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_RMX1971.mk
+    $(LOCAL_DIR)/dot_RMX1971.mk
--- dot_RMX1971.mk
+++ dot_RMX1971.mk
@@ -4,8 +4,8 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common cherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,7 +15,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := cherish_RMX1971
+PRODUCT_NAME := dot_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
